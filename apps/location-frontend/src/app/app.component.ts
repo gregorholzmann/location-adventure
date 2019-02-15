55e0073a8507110352d44b2fc439f8b0e3780ba0
Gregor Holzmann <debrisplease@gmail.com>
2019-02-15 13:05:24 -0700

    initial commit

---
+++ apps/location-frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'location-adventure-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+
+}
